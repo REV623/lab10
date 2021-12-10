5d92f2d3943b78d8796c36e232624f71a5f099ea
Reverse <jedsadakorn_k@cmu.ac.th>
2021-12-10 21:20:58 +0700

    completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,15 +2,45 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double loan,rate,pay;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
+
+	cout << setw(13) << left << "EndOfYear#"; 
+	cout << setw(13) << left << "PrevBalance"; 
+	cout << setw(13) << left << "Interest"; 
+	cout << setw(13) << left << "Total";
+	cout << setw(13) << left << "Payment";
+	cout << setw(13) << left << "NewBalance";
+	cout << "\n";
+
+	for(int i=1 ; true ; i++){ 
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << loan;
+		cout << setw(13) << left << loan*rate/100;
+		cout << setw(13) << left << loan+(loan*rate/100);
+		if(loan+(loan*rate/100) > pay){
+			cout << setw(13) << left << pay;
+			cout << setw(13) << left << (loan+(loan*rate/100))-pay;
+		}else{
+			cout << setw(13) << left << loan+(loan*rate/100);
+			cout << setw(13) << left << 0.00;
+			break;
+		}
+		cout << "\n";
+		loan = (loan+(loan*rate/100))-pay;
+	}
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
+/*	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
 	cout << setw(13) << left << "Total";
@@ -27,7 +57,7 @@ int main(){
 	cout << setw(13) << left << 1050.0;
 	cout << setw(13) << left << 100.0;
 	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << "\n";	*/
 	
 	return 0;
 }
